c477e68ffc5c2b07bd183f307c1c9f22f88ef14e
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-04-04 18:16:49 +0200

    feat: add k8sServiceHost & port for cilium correct cilium CNI installation

--- terraform/modules/cilium/main.tf
+++ terraform/modules/cilium/main.tf
@@ -7,6 +7,8 @@ resource "helm_release" "cilium_helm_chart" {
   values = [yamlencode({
     "ipam.operator.clusterPoolIPv4PodCIDRList" = var.pod_cidr
     kubeProxyReplacement                       = "true"
+    "k8sServiceHost"                           = "192.168.1.50"
+    "k8sServicePort"                           = "6443"
   })]
   create_namespace = true
 }
