e3560a25b2aaee1b829d0af074fa2c80ebf16e37
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-04-12 15:48:03 +0200

    feat: add argo-cd configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,10 @@ module "cilium" {
   pod_cidr       = var.pod_cidr
   cilium_version = var.cilium_version
 }
+
+
+module "argocd" {
+  source              = "./modules/argocd"
+  server_service_type = var.server_service_type
+  argocd_version      = var.argocd_version
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,16 @@ variable "cilium_version" {
   description = "configured cilium version"
   default     = "1.19.1"
 }
+
+
+variable "argocd_version" {
+  type        = string
+  description = "argocd version"
+  default     = "3.3.6"
+}
+
+variable "server_service_type" {
+  type        = string
+  description = "choose between NodePort & LoadBalancer"
+  default     = "NodePort"
+}
